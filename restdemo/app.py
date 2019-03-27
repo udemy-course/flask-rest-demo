b104ff2157a90d0d0156c0b8075bf6977fc662f3
Peng Xiao <xiaoquwl@gmail.com>
2019-03-27 23:18:15 +0100

    add user class
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

--- restdemo/app.py
+++ restdemo/app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
-from resource.user import User, UserList
-from resource.hello import Helloworld
+from restdemo.resource.user import User, UserList
+from restdemo.resource.hello import Helloworld
 
 
 app = Flask(__name__)
 api = Api(app)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///demo.db"
+db = SQLAlchemy()
+db.init_app(app)
 
 api.add_resource(Helloworld, '/')
 api.add_resource(User, '/user/<string:username>')
---
+++ restdemo/model/user.py
@@ -0,0 +1,8 @@
+from restdemo.app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(64), unique=True)

76aca4546767339bcea1cf10f6ebbfb538202ea0
Peng Xiao <xiaoquwl@gmail.com>
2019-04-07 19:09:46 +0200

    config improvement
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

--- restdemo/__init__.py
+++ restdemo/__init__.py
@@ -10,16 +10,16 @@ from restdemo.model.user import User as UserModel
 from restdemo.resource.user import User, UserList
 from restdemo.resource.hello import Helloworld
 from restdemo.resource.tweet import Tweet
-from restdemo.config import Config
+from restdemo.config import app_config
 
 jwt = JWT(None, UserModel.authenticate, UserModel.identity)
 
 
-def create_app():
+def create_app(config_name='development'):
 
     app = Flask(__name__)
     api = Api(app)
-    app.config.from_object(Config)
+    app.config.from_object(app_config[config_name])
     db.init_app(app)
     migrate = Migrate(app, db)
     jwt.init_app(app)
--- restdemo/config.py
+++ restdemo/config.py
@@ -2,10 +2,30 @@ from datetime import timedelta
 
 
 class Config:
-    # SQLALCHEMY_DATABASE_URI = "sqlite:///demo.db"
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/demo'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "flask123"
     JWT_EXPIRATION_DELTA = timedelta(seconds=300)
     JWT_AUTH_URL_RULE = '/auth/login'
+
+
+class TestingConfig(Config):
     JWT_AUTH_HEADER_PREFIX = 'FLASK'
+    SECRET_KEY = "flask123"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
+
+class DevelopmentConfig(Config):
+    JWT_AUTH_HEADER_PREFIX = 'FLASK'
+    SECRET_KEY = "flask123"
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/demo'
+
+
+class ProductionConfig(Config):
+    pass
+
+
+app_config = {
+    'testing': TestingConfig,
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}

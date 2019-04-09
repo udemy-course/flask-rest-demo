ab8ca3968dc2baa33ca6e850a9675b805e92d8e4
Peng Xiao <xiaoquwl@gmail.com>
2019-04-09 21:03:38 +0200

    add env to config.py
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

--- restdemo/config.py
+++ restdemo/config.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+import os
 
 
 class Config:
@@ -6,18 +7,17 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_EXPIRATION_DELTA = timedelta(seconds=300)
     JWT_AUTH_URL_RULE = '/auth/login'
+    JWT_AUTH_HEADER_PREFIX = os.environ.get('JWT_AUTH_HEADER_PREFIX', 'FLASK')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'flask123')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(Config):
-    JWT_AUTH_HEADER_PREFIX = 'FLASK'
-    SECRET_KEY = "flask123"
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
 
 
 class DevelopmentConfig(Config):
-    JWT_AUTH_HEADER_PREFIX = 'FLASK'
-    SECRET_KEY = "flask123"
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/demo'
+    DEBUG = True
 
 
 class ProductionConfig(Config):

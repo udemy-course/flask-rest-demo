179f8458d36b0c68223990aee4c19479cc1134d4
Peng Xiao <xiaoquwl@gmail.com>
2019-04-12 22:59:11 +0200

    update requirements.txt
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

--- restdemo/config.py
+++ restdemo/config.py
@@ -1,6 +1,8 @@
 from datetime import timedelta
 import os
 
+config_path = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
 
@@ -9,7 +11,7 @@ class Config:
     JWT_AUTH_URL_RULE = '/auth/login'
     JWT_AUTH_HEADER_PREFIX = os.environ.get('JWT_AUTH_HEADER_PREFIX', 'FLASK')
     SECRET_KEY = os.environ.get('SECRET_KEY', 'flask123')
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///" + os.path.join(config_path, 'twittor.db'))
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'

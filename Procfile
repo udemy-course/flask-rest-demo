7e94a1252fa7098cfeb4db207a21978cca4e3f67
Peng Xiao <xiaoquwl@gmail.com>
2019-04-12 22:56:17 +0200

    heroku deploy
    Signed-off-by: Peng Xiao <xiaoquwl@gmail.com>

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn -w 4 restdemo.wsgi:application
--- restdemo/config.py
+++ restdemo/config.py
@@ -9,8 +9,7 @@ class Config:
     JWT_AUTH_URL_RULE = '/auth/login'
     JWT_AUTH_HEADER_PREFIX = os.environ.get('JWT_AUTH_HEADER_PREFIX', 'FLASK')
     SECRET_KEY = os.environ.get('SECRET_KEY', 'flask123')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///" + os.path.join(config_path, 'twittor.db'))
 
 class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
